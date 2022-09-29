183d246c5920575d195ad9058e8a65ed9a7221ae
nikhildavid <51994571+nikhildavid@users.noreply.github.com>
2022-09-29 13:58:38 +0530

    Update Listeners.java

--- src/test/java/Listeners/Listeners.java
+++ src/test/java/Listeners/Listeners.java
@@ -6,9 +6,6 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 
 
 public class Listeners implements ITestListener{
